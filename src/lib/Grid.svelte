fd1f039c966f5351f0f94d56ec007037d3742507
Valerie <valerieknape@gmail.com>
2022-09-04 09:42:19 +0200

    Added colorchanging feature + correct choice count

--- src/lib/Grid.svelte
+++ src/lib/Grid.svelte
@@ -1,23 +1,34 @@
 <script>
 	let questions = ['a', 'b', 'c', 'd'];
-	import { questionNumber } from '$lib/stores.js';
+	import { questionNumber, correctAtFirst } from '$lib/stores.js';
 	import { page } from '$app/stores';
 
 	function check(i) {
-		console.log($page.data.questions[$questionNumber].answer);
-		console.log(questions[i]);
+		let element = document.getElementById(questions[i]);
+		let correct = document.getElementsByClassName('trueAnswer');
+		let failed = document.getElementsByClassName('falseAnswer');
 		if (questions[i] == $page.data.questions[$questionNumber].answer) {
-			$questionNumber += 1;
+			element.classList.add('trueAnswer');
+			setTimeout(() => {
+				if (failed.length == 0) {
+					$correctAtFirst += 1;
+				}
+				[...correct].forEach((e) => e.classList.remove('trueAnswer'));
+				[...failed].forEach((e) => e.classList.remove('falseAnswer'));
+				$questionNumber += 1;
+				console.log($correctAtFirst);
+			}, 1000);
 		} else {
+			element.classList.add('falseAnswer');
 		}
 	}
 </script>
 
 <div class="grid">
 	{#each questions as question, i}
-		<button class="questionBox" on:click={() => check(i)}
-			>{$page.data.questions[$questionNumber].choices[question]}</button
-		>
+		<button class="questionBox" id={questions[i]} on:click={() => check(i)}>
+			<div class="text">{$page.data.questions[$questionNumber].choices[question]}</div>
+		</button>
 	{/each}
 </div>
 
@@ -31,8 +42,21 @@
 	}
 	button {
 		font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+		cursor: pointer;
+	}
+	.text {
+		transition: all 0.2s ease-in-out;
+	}
+	button:hover .text {
+		transform: scale(1.4);
 	}
 	.questionBox {
 		border: black solid 1px;
+	}
+	:global(.falseAnswer) {
+		background-color: red;
+	}
+	:global(.trueAnswer) {
+		background-color: lightgreen;
 	}
 </style>
